30d1284bb8fb757cbf763f5c912e6e90d636e714
michischmidt <schmidt.m.dev@gmail.com>
2021-05-26 01:04:36 +0200

    finished calculation

--- klm.py
+++ klm.py
@@ -7,15 +7,16 @@ import sys
 from enum import Enum
 
 
-class OriginalDurations(Enum):
+class KierasDurations(Enum):
     k = 280
     p = 1100
     b = 100
     h = 400
     m = 1200
 
-# Extract every oparation for klm out of textfile
+
 def extractOperations():
+    # Extract every oparation for klm out of textfile
     operations = []
     try:
         f = open(sys.argv[1], "r")
@@ -27,21 +28,47 @@ def extractOperations():
             if (operation.split("#")[0] != ""):
                 operations.append(operation.split("#")[0].strip().lower())
 
-
-        print(operations)
-        print(OriginalDurations.k.value)
+        print(f"Found operations {operations}")
         return operations
     except:
         print("There was an error with reading from the operations file.")
         sys.exit(1)
 
 
+def calculate_klm(operations, factors):
+    # Calculate KLM estimated based on the given factors and operations
+    # Values Enum check is from Stackoverflow:
+    # https://stackoverflow.com/questions/43634618/
+    # how-do-i-test-if-int-value-exists-in-python
+    # -enum-without-using-try-catch
+    values = set(item.name for item in factors)
+    estimate = 0
+    singleInstructions = []
+
+    for operation in operations:
+        for index, char in enumerate(operation):
+            digitFactor = ""
+            # if digit is found, complete digit, save as factor and multiple next instruction with it
+            if (char.isdigit()):
+                j = index
+                while(operation[j].isdigit()):
+                    digitFactor += operation[j]
+                    j += 1
+            if (digitFactor != ""):
+                digitFactor = int(digitFactor.strip())
+                if (char in values):
+                    estimate += factors[char].value * digitFactor
+
+            # if char is in enum add its value to estimate
+            if (char in values):
+                estimate += factors[char].value
+
+    return estimate
+
+
 if __name__ == "__main__":
     operations = extractOperations()
-    # origialEstimate = calculate_klm_orginal(operations)
-
-    # singleInstructions = []
-    #     for operation in operations:
-    #         for char in operation: 
-    #             if (char.isDigit()):
-    # median = calculate_klm_orginal(nums)
+    origialEstimate = calculate_klm(operations, KierasDurations)
+    # origialEstimate = calculate_klm(operations, false)
+    print(
+        f"With original estimates (Card et. al): {origialEstimate} Milliseconds")
